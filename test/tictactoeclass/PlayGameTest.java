1078c53761c273671c6808c26989d47049cb1f82
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2021-04-21 15:41:21 -0400

    Tic tac toe j unit test cases (#12)
    * TDD Spring 2021 demo - cleaned up comments
    
    * Delete .jacocoverage/report.html/tictactoeclass directory
    
    Java Code Coverage files not needed on github

--- test/tictactoeclass/PlayGameTest.java
+++ test/tictactoeclass/PlayGameTest.java
@@ -31,7 +31,79 @@ public class PlayGameTest {
     }
 
     /**
+     * Test Board class isWinner(symbol) method
+     * Verify a number of different win scenarios for both symbols (return true)
+     * Also verify non-winning scenarios (return false)
+     * 
+     * Note: test cases assume X initially goes 1st and
+     *   subsequent games - loser goes 1st
+     * ToDo: Better to create a test case to verify assumptions and/or
+     *   ToDo: specify who player with 1st move at the start of each game
+     */
+    @Test
+    public void playToWinXOnBackwardsDiag() {
+        System.out.println("***Testing: X to win on backward diagnonal");
+        PlayGame play = new PlayGame(drop);
+        play.playGame(0,0);  // X goes 1st
+        play.playGame(0,1);
+        play.playGame(1,1);
+        play.playGame(1,0);
+        assertEquals(play.isWinner(Board.Symbols.X), false);
+        assertEquals(play.isWinner(Board.Symbols.O), false);
+        play.playGame(2,2);
+        assertEquals(play.isWinner(Board.Symbols.X), true);
+        assertEquals(play.isWinner(Board.Symbols.O), false);
+        
+        // Reset - Since X won, O will go first
+        //   test O winning on backwards diagonal
+        System.out.println("***Testing: O to win on backward diagnonal");
+        play.resetBoard();
+        assertEquals(play.isWinner(Board.Symbols.X), false);
+        play.playGame(0,0);
+        play.playGame(0,1);
+        play.playGame(1,1);
+        play.playGame(1,0);
+        assertEquals(play.isWinner(Board.Symbols.X), false);
+        assertEquals(play.isWinner(Board.Symbols.O), false);
+        play.playGame(2,2);
+        assertEquals(play.isWinner(Board.Symbols.O), true);
+        assertEquals(play.isWinner(Board.Symbols.X), false);
+    }
+    
+    @Test
+    public void playToWinXOnForwardDiag() {
+        System.out.println("***Testing: X to win on forward diagnonal");
+        PlayGame play = new PlayGame(drop);
+        play.playGame(2,0);
+        play.playGame(0,1);
+        play.playGame(1,1);
+        play.playGame(1,0);
+        assertEquals(play.isWinner(Board.Symbols.X), false);
+        assertEquals(play.isWinner(Board.Symbols.O), false);
+        play.playGame(0,2);
+        assertEquals(play.isWinner(Board.Symbols.X), true);
+        assertEquals(play.isWinner(Board.Symbols.O), false);
+    }
+    
+    @Test
+    public void playToWinXOnRow0() {
+        System.out.println("***Testing: X to win on 1st row");
+        PlayGame play = new PlayGame(drop);
+        play.playGame(0,0);
+        play.playGame(1,0);
+        play.playGame(0,1);
+        play.playGame(1,1);
+        assertEquals(play.isWinner(Board.Symbols.X), false);
+        assertEquals(play.isWinner(Board.Symbols.O), false);
+        play.playGame(0,2);
+        assertEquals(play.isWinner(Board.Symbols.X), true);
+        assertEquals(play.isWinner(Board.Symbols.O), false);
+    }
+    
+    /**
      * Test of playGame method, of class PlayGame.
+     * Test case designed to test InterProcess Communication (drop.take)
+     * Verified to work as expected, disabled test case (commented out @Test)
      */
     // @Test
     public void testPlayGame() {
@@ -46,34 +118,6 @@ public class PlayGameTest {
         }
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
-    }
-    
-    @Test
-    public void playToWinXOnDiag() {
-        PlayGame play = new PlayGame(drop);
-        play.playGame(0,0);
-        play.playGame(0,1);
-        play.playGame(1,1);
-        play.playGame(1,0);
-        assertEquals(play.isWinner(Board.Symbols.X), false);
-        assertEquals(play.isWinner(Board.Symbols.O), false);
-        play.playGame(2,2);
-        assertEquals(play.isWinner(Board.Symbols.X), true);
-        assertEquals(play.isWinner(Board.Symbols.O), false);
-        
-        // Reset - Since X won, O will go first
-        //   reverse win check
-        play.resetBoard();
-        assertEquals(play.isWinner(Board.Symbols.X), false);
-        play.playGame(0,0);
-        play.playGame(0,1);
-        play.playGame(1,1);
-        play.playGame(1,0);
-        assertEquals(play.isWinner(Board.Symbols.X), false);
-        assertEquals(play.isWinner(Board.Symbols.O), false);
-        play.playGame(2,2);
-        assertEquals(play.isWinner(Board.Symbols.O), true);
-        assertEquals(play.isWinner(Board.Symbols.X), false);
     }
     
 }
